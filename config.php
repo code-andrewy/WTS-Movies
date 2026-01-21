e45874ca97fc831a1c44b6940fc78bc81722f6a3
code-andrewy <148534180+code-andrewy@users.noreply.github.com>
2026-01-21 05:55:05 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// 1. YOUR API KEY (Get this from themoviedb.org)
+$api_key = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDBiMDIzOWQzNGY5YzI0Yjc2MzM5Mjg2YjNlOWNiMSIsIm5iZiI6MTc0ODUwMzgzMS43NjMsInN1YiI6IjY4MzgwZDE3MDc5YTQyZTI4NzAzODYyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ykvYVrw8wjBdjkeb-71y1n1Z8ng3xE5ciodJ6FZgrNw'; 
+
+// 2. YOUR SITE URL (Crucial for the sitemap and images)
+$SiteURL = 'https://google.com'; 
+
+// 3. YOUR SITE NAME
+$SiteTitle = 'WTS-Movies';
+
+// 4. API CONFIG (Leave these unless they are broken)
+$APIbaseURL = 'https://api.themoviedb.org/3/';
+$language = '&language=en-US';
+$trendingmovieweek = 'trending/movie/week?api_key=';
+$trendingtvweek = 'trending/tv/week?api_key=';
